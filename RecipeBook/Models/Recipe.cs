de1852141e117d87a3996ff335f73aade5008fc5
Joey Palchak <joey.palchak@gmail.com>
2023-10-17 09:54:00 -0700

    add ViewModels + validation for Recipe

--- RecipeBook/Models/Recipe.cs
+++ RecipeBook/Models/Recipe.cs
@@ -1,15 +1,19 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace RecipeBook.Models;
 
 public class Recipe
 {
   public int RecipeId { get; set; }
-
+  [Required(ErrorMessage = "The recipe's name can't be empty!")]
   public string Name { get; set; }
+  [Required(ErrorMessage = "The recipe's instructions can't be empty!")]
   public string Instructions { get; set; }
+  [Required(ErrorMessage = "The recipe's ingredients can't be empty!")]
   public string Ingredients { get; set; }
 
+  [Range(1, int.MaxValue, ErrorMessage = "You must add your recipe to a category. Have you created a category yet?")]
   public int CategoryId { get; set; }
   // Reference Navigation Property
   public Category Category { get; set; }
--- RecipeBook/ViewModels/LoginViewModel.cs
+++ RecipeBook/ViewModels/LoginViewModel.cs
@@ -4,5 +4,12 @@ namespace RecipeBook.ViewModels;
 
 public class LoginViewModel
 {
-  
+  [Required]
+  [EmailAddress]
+  [Display(Name = "Email Address")]
+  public string Email { get; set; }
+
+  [Required]
+  [DataType(DataType.Password)]
+  public string Password { get; set; }
 }
